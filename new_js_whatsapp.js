700b201dbe1d15169f2d4dcc7649ed35bb1d9423
Manikanta <mani.iiit123@gmail.com>
2016-06-22 16:22:24 +0530

    Code added for timeline and group activity extraction
    Summary: Code added for timeline and group activity extraction
    
    Test Plan: Doc created of all the activity
    
    Reviewers: madhavan93, abhilashi
    
    Differential Revision: http://107.167.176.165/D1147

--- new_js_whatsapp.js
+++ new_js_whatsapp.js
@@ -49,9 +49,11 @@ setInterval(function() {
   if ((body.scrollTop + body.clientHeight != body.scrollHeight) && (groups == 0 && chats == 0)){
       scroll_content();
   }  else if( (body.scrollTop + body.clientHeight == body.scrollHeight) && count == 0) { 
-    scroll_content(); count = 1; calculate_groups_and_chats();
+      scroll_content(); 
+      count = 1; 
+      calculate_groups_and_chats();
   }
-}, 1500);
+}, 3000);
 
 
 function scroll_content() {
@@ -62,9 +64,15 @@ function scroll_content() {
     var status = check_duplicates(chat_divs[i]);
     if (status === "false") {
       var node = chat_divs[i];
+      var muted = node.getElementsByClassName('icon icon-meta icon-muted');
+      mute_status = false;
+      if (muted.length != 0) {
+        mute_status = true;
+      }
       var object = {
         key : get_key(node),
-        object : node
+        object : node,
+        mute_status : mute_status
       }
       chats_array.push(object);
     }
@@ -95,4 +103,101 @@ function calculate_groups_and_chats() {
     }
   }
   console.log("Number of groups = " + groups + " and number of chats = " + chats + " and number of muted groups = " + muted_groups);
+}
+
+
+// Group
+var users = [], msg_in = [], msg_out = [];
+var timeline_values = new Array(24).fill(0); 
+function update_user(user_passed) {
+  var user = user_passed.toString();
+  // check if it exists 
+  var user_index = -1;
+  for (var i=0; i<users.length; i++) {
+    if (users[i].name == user) {
+      user_index = i;
+      break;
+    }
+  }
+  if (user_index == -1) {
+    var object = {};
+    object['name'] = user;
+    object['messages'] = 1;
+    users.push(object);
+  } else {
+    users[user_index]['messages'] += 1;
+  }
+}
+
+/**
+ * @param : text of time. 
+ *   return : hour (integer)
+ */
+function extract_hour(time_passed) {
+  var time = time_passed.toString();
+  var hour_extraction = time.search(":");
+  var hour_string = "";
+  var offset = 0;
+  var meridem = time.search("PM");
+  if (meridem != -1) {
+    offset = 12;
+  } 
+  for (var i=0; i<hour_extraction; i++){
+    hour_string = hour_string + time[i];
+  } 
+  console.log("Hour String = " + hour_extraction + " hour string = " + hour_string);
+  return parseInt(hour_string) + offset; 
+}
+
+function update_messages_by_you() {
+  var object = {};
+  object["name"] = "You";
+  object['messages'] = msg_out.length;
+  users.push(object);
+}
+
+
+function print_list() {
+  users.sort(function(b, a){
+      return a.messages-b.messages;
+  })
+  for (var i=0; i<users.length; i++) {
+    console.log(users[i].name + " : " + users[i].messages);
+  }
+
+  console.log("Total messages : " + msg_in.length);
+  console.log("Total timeline values : " + timeline_values);
+}
+
+
+
+/* TODO: Messages count:-  Total groups and activity in that groups (class: message_in,  message_out)
+ *  @param: null
+ *  return : object{messages_out : <int>, messages_out : <int>, muted_status : <boolean>} 
+ */
+function compute_activity() {
+  users = [];
+  msg_in = document.getElementsByClassName('message-in');
+  msg_out = document.getElementsByClassName('message-out');
+  
+
+  for (var i=0; i<msg_in.length; i++) { 
+    var x = msg_in[i].getElementsByClassName('number text-clickable');  
+    if (x.length == 0) {
+      var y = msg_in[i].getElementsByClassName('text-clickable');  
+      if (y.length != 0){
+          // Actor update 
+          update_user(y[0].innerText);
+        }
+      } else {
+        update_user(x[0].innerText);
+      } 
+
+      // Extract hours 
+      var time = msg_in[i].getElementsByClassName('message-datetime')[0].innerText;
+      console.log("Time = " + time + "Time extracted = " + extract_hour(time));
+      timeline_values[extract_hour(time)-1] += 1; 
+    }
+  update_messages_by_you();
+  
 }
